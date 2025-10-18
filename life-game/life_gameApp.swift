65a8fad0457b0f2d33d94ffb98c3bd2aaaf84512
Kevin Hill <kevinhill@live.com>
2025-10-18 11:03:12 +0200

    Initial Commit

---
+++ life-game/life_gameApp.swift
@@ -0,0 +1,32 @@
+//
+//  life_gameApp.swift
+//  life-game
+//
+//  Created by Kevin Hill on 10/18/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct life_gameApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ life-gameTests/life_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  life_gameTests.swift
+//  life-gameTests
+//
+//  Created by Kevin Hill on 10/18/25.
+//
+
+import Testing
+@testable import life_game
+
+struct life_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
